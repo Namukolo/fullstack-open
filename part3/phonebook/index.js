7121a5829116b5f7686cbf3e972c419f0027807b
unknown <namukolo@bitcube.tech>
2025-07-07 11:25:06 +0200

    feat(part3): complete exercises 3.1 - 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.json())
+
+const persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+    const requestTime = new Date();
+
+
+    const responseContent = `<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${requestTime}</p>
+    </div>`
+
+    response.send(responseContent)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const requestId = request.params.id;
+
+    const person = persons.find(person => person.id === requestId);
+
+    if (!person) { response.status(404).end(); }
+
+    response.json(person);
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const requestId = request.params.id;
+    const isID = persons.find(requestId);
+
+    if (!isID) {
+        response.status(404).end();
+    }
+
+    const filteredPersons = persons.filter(person => person.id !== requestId);
+
+    response.json(filteredPersons);
+})
+
+app.post('/api/persons', (request, response) => {
+    try {
+        const newPerson = request.body;
+        const { name, number } = newPerson;
+        const entryExists = persons.find(person => person.name.toLowerCase() === name.toLowerCase());
+
+        if (!name || !number) {
+            const errorMsg = number ? 'number is missing' : 'name is missing';
+            throw new Error(errorMsg);
+        }
+
+
+        if (entryExists) {
+            throw new Error('name must be unique');
+        }
+
+        const newPersonId = Math.floor(Math.random() * 1000);
+        newPerson.id = newPersonId;
+
+        persons.push(newPerson);
+
+        response.status(201).json({message: 'Entry added successfully', data: newPerson});
+    } catch (error) {
+        response.status(400).json({ error: error.message })
+    }
+})
+
+
+app.listen('3001', () => {
+    console.log('we got the server running baby');
+})
