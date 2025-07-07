8aaa3aa4c16ead9cd1a19532135d8e596b9f26fa
unknown <namukolo@bitcube.tech>
2025-07-07 12:41:18 +0200

    feat(part3): complete exercises 3.7 - 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 
 
 const app = express();
+
+morgan.token('request-data', function getRequestData(req) {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :request-data`));
 
 app.use(express.json())
 
